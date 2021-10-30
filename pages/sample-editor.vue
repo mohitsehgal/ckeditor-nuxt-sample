e12e39bb439cbde32c49d87ad7790b2dc9052cc3
Mohit Sehgal <sehgal.mohit29@gmail.com>
2021-10-31 00:06:33 +0530

    Using Refactored rich-editor

---
+++ pages/sample-editor.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1 class="container mt-3 mb-3">Sample CKEditor with Nuxt.js</h1>
+    <div class="container">
+        <rich-editor  :config="editorConfig" v-model="editorInput" />
+    </div>
+    <div class="container mt-3">
+      <div class="row">
+        <h2 class="col-md-12">Output</h2>
+        <div>{{ editorInput }}</div>
+      </div>
+    </div>
+    <div class="container mt-3">
+      <div class="row">
+        <h2 class="col-md-12">Preview</h2>
+        <div v-html="editorInput"></div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+import RichEditor from '../components/rich-editor.vue';;
+
+export default {
+  components: {
+    richEditor: RichEditor
+  },
+  data() {
+    return {
+      editorInput: '',
+      editorConfig: {
+        width: 'auto',
+        plugins: [
+         'Bold',
+          'Link',
+          'List',
+          'FontSize',
+          `MathType`,
+          `Image`,
+          `ImageUpload`,
+          'SimpleUploadAdapter',
+          `MediaEmbed`
+        ],
+         simpleUpload: {
+          // The URL that the images are uploaded to.
+          uploadUrl: 'https://yourwebsite.com/api/upload-image',
+        },
+      }
+    }
+  },
+}
+</script>
